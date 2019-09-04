5b8c8e9fb84414334084d795b17acada19705822
baztiann <54882236+baztiann@users.noreply.github.com>
2019-09-04 20:19:26 +0700

    Create Testi.py

---
+++ Testi.py
@@ -0,0 +1,218 @@
+clear
+figlet Brother | lolcat
+echo " 
+\033[1;97m█████████
+\033[1;97m█▄█████▄█      ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+\033[1;97m█ ▲▲▲▲▲            Tools Brother 
+\033[1;97m█ ▲▲▲▲▲        «----------✧----------»
+\033[1;97m█████████ 
+\033[1;97m ██ ██
+\033[1;93m╔════════════════════════════════════════════╗
+\033[1;93m║\033[1;96m* \033[1;93mAuthor  \033[1;93m: \033[1;93mBrother•|XXX Baztian XXX  \033[1;93m              ║
+\033[1;93m║\033[1;96m* \033[1;93mGitHub  \033[1;93m: \033[1;93m\033[4mMasi Rahasia Script Nya😑\033[0m \033[1;93m      ║
+\033[1;93m╚════════════════════════════════════════════╝""" | lolcat
+
+sleep 2
+
+echo "[ 01 ] Virtex
+[ 02 ] Santri Fbguard
+[ 03 ] Fbf
+[ 04 ] Crack Fb
+[ 05 ] Fake Fb
+[ 06 ] Unfired
+[ 07 ] Lacak Ip
+[ 08 ] Auto Mbf
+[ 09 ] Spam
+[ 10 ] Dark-Fb
+[ 11 ] Phising
+[ 12 ] Dark Mod2
+[ 13 ] Dark 3
+[ 14 ] Vip-1.9
+[ 15 ] exit" | lolcat
+echo "[*] Pilih nomor : " | lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+apt update && apt upgrade
+apt install git
+apt install curl
+apt install figlet
+apt install ruby
+gem install lolcat
+git clone https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+chmod +x virtex.sh
+sh virtex.sh
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+pkg install git
+git clone https://github.com/SantriCyber/santri-fbguard
+ls
+cd santri-fbguard
+ls
+php santri-fbguard.php
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+clear
+apt update && apt upgrade 
+pkg install git 
+pkg install python2 
+pip2 install mechanize 
+pip2 install requests 
+git clone https://github.com/rinaid/Fmbf 
+cd Fmbf 
+python2 Fmbf.py
+fi
+
+if [ $nomor = 4 ] || [ $nomor = 04 ]
+then
+clear
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/EgiRecod/crackfb
+cd crackfb
+python2 crack.py
+fi
+
+if [ $nomor = 5 ] || [ $nomor = 05 ]
+then
+clear
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/EgiRecod/FakeFB
+cd FakeFB
+python2 fake.py
+fi
+
+if [ $nomor = 6 ] || [ $nomor = 06 ]
+then
+clear
+apt update && apt upgrade -y
+apt inatall python2 -y
+apt install git -y
+git clone https://github.com/Unikers71/unfriend
+cd unfriend
+python2 unfriend.py
+fi
+
+if [ $nomor = 7 ] || [ $nomor = 07 ]
+then
+clear
+pkg update && pkg upgrade -y
+pkg install python2 -y
+pkg install git -y
+pip2 install requests
+git clone https://github.com/unikers71/lacakIP
+cd lacakIP
+python2 IP.py
+fi
+
+if [ $nomor = 8 ] || [ $nomor = 08 ]
+then
+clear
+pkg update && pkg upgrdae -y
+pkg install python2 -y
+pkg install git -y
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/unikers71/autombf
+cd autombf
+python2 mbf.py
+fi
+
+if [ $nomor = 9 ] || [ $nomor = 09 ]
+then
+clear
+pkg update && pkg upgrade -y
+pkg install git -y
+pkg install python2 -y
+pip2 install requests
+git clone https://github.com/unikers71/spammer
+cd spammer
+python2 spam.py
+fi
+
+if [ $nomor = 10 ] || [ $nomor = 10 ]
+then
+clear
+apt update && apt upgrade -y
+apt install python2 -y
+apt install git -y
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/unikers71/darkfb
+cd darkfb
+python2 darkfb.py
+fi
+
+if [ $nomor = 11 ] || [ $nomor = 11 ]
+then
+clear
+apt update && apt upgrade
+pkg install ruby toilet figlet
+gem install lolcat
+apt-get install bash
+pkg install git
+git clone https://github.com/ridwanirawan/PiPhish
+cd PiPhish
+ls
+sh run.sh
+fi
+
+if [ $nomor = 12 ] || [ $nomor = 12 ]
+then
+clear
+pkg upgrade && pkg update 
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pkg install git
+git clone https://github.com/cyber2611/cyber2000
+ls
+cd cyber2000
+ls
+python2 Dark2.py
+fi
+
+if [ $nomor = 13 ] || [ $nomor = 13 ]
+then
+clear
+pkg update
+pkg upgrade
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+pkg install git
+git clone https://github.com/cyber2611/dark3
+ls
+cd dark3
+ls
+python2 coding2.py
+fi
+
+if [ $nomor = 14 ] || [ $nomor = 14 ]
+then
+clear
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+pkg install git 
+git clone https://github.com/cyber2611/BHV26
+ls
+cd BHV26
+ls
+python2 vip-1.9.py
+fi
+
+if
